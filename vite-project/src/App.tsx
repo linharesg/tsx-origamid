972f830552d57acf7454d8b511e4ef42f37fed99
Gabriel Linhares <linhares.gs96@gmail.com>
2025-11-18 18:47:38 -0300

    0305-3 useFetch

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import useFetch from "./Hooks/useFetch";
 import Input from "./Input";
 
@@ -10,12 +11,36 @@ type Produto = {
 };
 
 function App() {
+  const [id, setId] = React.useState("P001");
   const produtos = useFetch<Produto[]>("https://data.origamid.dev/produtos/");
-
+  const produto = useFetch<Produto>(`https://data.origamid.dev/produtos/${id}`);
   return (
-    <div>
-      <Input label="Nome" id="name" type=""></Input>
-    </div>
+    <>
+      <section className="flex">
+        {produtos.data &&
+          produtos.data.map((produto) => (
+            <button
+              onClick={() => setId(produto.id)}
+              style={{ fontSize: "1rem" }}
+              key={produto.id}
+            >
+              {produto.id}
+            </button>
+          ))}
+      </section>
+      <div>
+        {produto.data && (
+          <ul>
+            <li>id: {produto.data.id}</li>
+            <li>nome: {produto.data.nome}</li>
+            <li>quantidade: {produto.data.quantidade}</li>
+          </ul>
+        )}
+      </div>
+      <div>
+        <Input label="Nome" id="name" type=""></Input>
+      </div>
+    </>
   );
 }
 
